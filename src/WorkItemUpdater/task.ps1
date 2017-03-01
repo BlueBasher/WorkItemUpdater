24607870ad1d187de98403e5dad37711bbb129da
Marco van Kimmenade <marco@bluebasher.com>
2017-03-01 19:49:44 +0100

    1.2.0
    - Add Build as Link
    - Update Assigned To with Build Requester

--- src/WorkItemUpdater/task.ps1
+++ src/WorkItemUpdater/task.ps1
@@ -21,8 +21,6 @@ $onAssemblyResolve = [System.ResolveEventHandler]{
 			return [System.Reflection.Assembly]::LoadFrom($httpFormatingDll)
 		}
 	}
-
-    Write-Verbose "Unable to resolve assembly name '$($e.Name)'"
     return $null
 }
 [System.AppDomain]::CurrentDomain.add_AssemblyResolve($onAssemblyResolve)
@@ -31,56 +29,129 @@ Import-Module .\ps_modules\VstsTaskSdk\VstsTaskSdk.psm1 -Verbose:$true
 
 Trace-VstsEnteringInvocation $MyInvocation
 
+function Update-WorkItem {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [Microsoft.TeamFoundation.WorkItemTracking.WebApi.WorkItemTrackingHttpClient]$workItemTrackingHttpClient,
+        [Parameter(Mandatory = $true)]
+        [int]$workItemId,
+        [Parameter(Mandatory = $true)]
+        [int]$buildId,
+        [Parameter(Mandatory = $true)]
+        [string]$workItemType,
+        [Parameter(Mandatory = $true)]
+        [string]$workItemState,
+        [Parameter(Mandatory = $true)]
+        [string]$workItemDone,
+        [Parameter(Mandatory = $true)]
+        [bool]$linkBuild,
+        [Parameter(Mandatory = $true)]
+        [string]$requestedFor,
+        [Parameter(Mandatory = $true)]
+        [string]$updateAssignedTo)
+
+	Write-VstsTaskDebug -Message "Found WorkItemRef: $($workItemId)"
+	$workItem = $workItemTrackingHttpClient.GetWorkItemAsync($workItemId, $null, $null, [Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models.WorkItemExpand]::Relations).Result
+	Write-VstsTaskDebug -Message "Found WorkItem: $($workItem.Id)"
+	if ($workItem.Fields["System.WorkItemType"] -eq $workItemType)
+	{
+		Write-Host "Updating WorkItem $($workItem.Id)"
+
+		$kanbanColumn = $workItem.Fields.Keys | Where-Object { $_.EndsWith("Kanban.Column") }
+		Write-VstsTaskDebug -Message "Found KanbanColumn: $($kanbanColumn)"
+		$kanbanField = "/fields/$($kanbanColumn).Done"
+
+		$patch = New-Object Microsoft.VisualStudio.Services.WebApi.Patch.Json.JsonPatchDocument
+		$columnOperation = New-Object Microsoft.VisualStudio.Services.WebApi.Patch.Json.JsonPatchOperation
+		$columnOperation.Operation = [Microsoft.VisualStudio.Services.WebApi.Patch.Operation]::Add
+		$columnOperation.Path = "/fields/System.State"
+		$columnOperation.Value = $workItemState
+		$patch.Add($columnOperation)
+		Write-VstsTaskDebug -Message "Patch: $($columnOperation.Path) $($columnOperation.Value)"
+
+		$columnDoneOperation = New-Object Microsoft.VisualStudio.Services.WebApi.Patch.Json.JsonPatchOperation
+		$columnDoneOperation.Operation = [Microsoft.VisualStudio.Services.WebApi.Patch.Operation]::Add
+		$columnDoneOperation.Path = $kanbanField
+		$columnDoneOperation.Value = $workItemDone
+		$patch.Add($columnDoneOperation)
+		Write-VstsTaskDebug -Message "Patch: $($columnDoneOperation.Path) $($columnDoneOperation.Value)"
+
+		if ($linkBuild -eq $true)
+		{
+			$buildRelationUrl = "vstfs:///Build/Build/$buildId"
+			$buildRelation = $workItem.Relations | Where-Object { $_.Url -eq $buildRelationUrl }
+			if ($buildRelation -eq $null) {
+				Write-Host "Linking Build $($buildId) to WorkItem $($workItem.Id)"
+				$linkBuildOperation = New-Object Microsoft.VisualStudio.Services.WebApi.Patch.Json.JsonPatchOperation
+				$linkBuildOperation.Operation = [Microsoft.VisualStudio.Services.WebApi.Patch.Operation]::Add
+				$linkBuildOperation.Path = "/relations/-"
+				$linkBuildOperation.Value = @{
+						Rel = "ArtifactLink"
+						Url = $buildRelationUrl
+						Attributes = @{
+							name = "Build"
+						}
+					}
+				$patch.Add($linkBuildOperation)
+				Write-VstsTaskDebug -Message "Patch: $($linkBuildOperation.Path) $($buildRelationUrl)"
+			}
+			else {
+				Write-Host "Build $($buildId) already linked to WorkItem $($workItem.Id)"
+			}
+		}
+		if ($updateAssignedTo -eq "Always" -or ($updateAssignedTo -eq "Unassigned" -and $workItem.Fields.ContainsKey("System.AssignedTo") -eq $false))
+		{
+			$assignedToOperation = New-Object Microsoft.VisualStudio.Services.WebApi.Patch.Json.JsonPatchOperation
+			$assignedToOperation.Operation = [Microsoft.VisualStudio.Services.WebApi.Patch.Operation]::Add
+			$assignedToOperation.Path = "/fields/System.AssignedTo"
+			$assignedToOperation.Value = $requestedFor
+			$patch.Add($assignedToOperation)
+			Write-VstsTaskDebug -Message "Patch: $($assignedToOperation.Path) $($assignedToOperation.Value)"
+		}
+
+		$updatedWorkItem = $workItemTrackingHttpClient.UpdateWorkItemAsync($patch, $workItem.Id).Result
+
+		Write-Host "WorkItem $($updatedWorkItem.Id) updated to $($workItemState) $($workItemDone)"
+	}
+	else
+	{
+		Write-VstsTaskDebug -Message "Skipped $($workItem.Fields['System.WorkItemType']) WorkItem: $($workItem.Id)"
+	}
+}
+
 try {
     $buildId = Get-VstsTaskVariable -Name "Build.BuildId"
 	$projectId = Get-VstsTaskVariable -Name "System.TeamProjectId"
+	$requestedFor = Get-VstsTaskVariable -Name "Build.RequestedFor"
 	$workItemType = Get-VstsInput -Name "workItemType"
 	$workItemState = Get-VstsInput -Name "workItemState"
 	$workItemDone = Get-VstsInput -Name "workItemDone" -AsBool 
+	$linkBuild = Get-VstsInput -Name "linkBuild" -AsBool
+	$updateAssignedTo = Get-VstsInput -Name "updateAssignedTo"
 
     Write-VstsTaskDebug -Message "BuildId $buildId"
     Write-VstsTaskDebug -Message "ProjectId $projectId"
+    Write-VstsTaskDebug -Message "requestedFor $requestedFor"
     Write-VstsTaskDebug -Message "workItemType $workItemType"
     Write-VstsTaskDebug -Message "WorkItemState $workItemState"
     Write-VstsTaskDebug -Message "WorkItemDone $workItemDone"
+    Write-VstsTaskDebug -Message "updateAssignedTo $updateAssignedTo"
 
 	$workItemTrackingHttpClient = Get-VssHttpClient -TypeName Microsoft.TeamFoundation.WorkItemTracking.WebApi.WorkItemTrackingHttpClient
     $buildHttpClient = Get-VssHttpClient -TypeName Microsoft.TeamFoundation.Build.WebApi.BuildHttpClient
 	$workItemsRefs = $buildHttpClient.GetBuildWorkItemsRefsAsync($projectId, $buildId).Result
 	foreach ($workItemRef in $workItemsRefs)
 	{
-		Write-VstsTaskDebug -Message "Found WorkItemRef: $($workItemRef.Id)"
-		$workItem = $workItemTrackingHttpClient.GetWorkItemAsync($workItemRef.Id).Result
-		Write-VstsTaskDebug -Message "Found WorkItem: $($workItem.Id)"
-		if ($workItem.Fields["System.WorkItemType"] -eq $workItemType)
-		{
-			$kanbanColumn = $workItem.Fields.Keys | where { $_.EndsWith("Kanban.Column") }
-			Write-VstsTaskDebug -Message "Found KanbanColumn: $($kanbanColumn)"
-			$kanbanField = "/fields/$($kanbanColumn).Done"
-
-			$patch = New-Object Microsoft.VisualStudio.Services.WebApi.Patch.Json.JsonPatchDocument
-			$columnOperation = New-Object Microsoft.VisualStudio.Services.WebApi.Patch.Json.JsonPatchOperation
-			$columnOperation.Operation = 0
-			$columnOperation.Path = "/fields/System.State"
-			$columnOperation.Value = $workItemState
-			$patch.Add($columnOperation)
-			Write-VstsTaskDebug -Message "Patch: $($columnOperation.Path) $($columnOperation.Value)"
-
-			$columnDoneOperation = New-Object Microsoft.VisualStudio.Services.WebApi.Patch.Json.JsonPatchOperation
-			$columnDoneOperation.Operation = 0
-			$columnDoneOperation.Path = $kanbanField
-			$columnDoneOperation.Value = $workItemDone
-			$patch.Add($columnDoneOperation)
-			Write-VstsTaskDebug -Message "Patch: $($columnDoneOperation.Path) $($columnDoneOperation.Value)"
-
-			Write-Host "Updating WorkItem $($workItem.Id)"
-			$updateResult = $workItemTrackingHttpClient.UpdateWorkItemAsync($patch, $workItem.Id).Result
-			Write-Host "WorkItem $($workItem.Id) updated to $($workItemState) $($workItemDone)"
-		}
-		else
-		{
-			Write-VstsTaskDebug -Message "Skipped $($workItem.Fields['System.WorkItemType']) WorkItem: $($workItem.Id)"
-		}
+		Update-WorkItem -workItemTrackingHttpClient $workItemTrackingHttpClient `
+        	-workItemId $workItemRef.Id `
+        	-buildId $buildId `
+			-workItemType $workItemType `
+			-workItemState $workItemState `
+			-workItemDone $workItemDone `
+			-linkBuild $linkBuild `
+			-requestedFor $requestedFor `
+			-updateAssignedTo $updateAssignedTo
 	}
 }
 catch {
--- src/overview.md
+++ src/overview.md
@@ -1,12 +1,22 @@
 ﻿# WorkItem Updater
 
 ## Overview
-The WorkItem Updater task updatesthe state for workitems linked to a build.  
-Using this task developers don't need to manually keep track of the state.  
-An example would be to set the state to 'Resolved' as the last step of a build and to 'Deployed' as the last step of a release.  
-With this task it is possible to create an automated Kanban board as shown [here](https://bluebasher.com/Blog/Post/2017/1/devops_with_vsts_the_first_way_kanban#automated_kanban)  
-
+The WorkItem Updater task can update the following workitem fields during a build or release:
+- Update the state for workitems linked to a build.  
+- Update the assignee for workitems linked to a build.  
+- Add the build as Development Link to the workitems.
+  
+By adding this task to specific milestones in a build/release pipeline, you can create an automated kanban board.  
+As an example would be to set the state to 'Resolved' as the last step of a build and to 'Deployed' as the last step of a release.  
+With this task the state of workitems is always reflecting reality and developers don't need to manually update workitems anymore.  
+  
+A preview of what the task can do, can be seen in this recording:  
+  
+![AutoKanban](img/AutoKanban.gif)  
+  
 ## Settings
+![settings](img/Settings.png)  
+  
 The task requires the following settings:
 - WorkItem Type
   - Only linked workitems of this type will be updated.
@@ -14,3 +24,10 @@ The task requires the following settings:
   - The state that the workitem should be updated to.
 - Move to board column Done
   - If the workitem is displayed in a Kanban column that has been split into Doing and Done, this indicates if the workitem should be moved to the Done column.
+- Add Build as Development link
+  - If checked the build will be added as a Development link to the WorkItem.
+- Update 'Assigned To' with requester of the build
+  - The following options are available to configure when the "Assigned To" field should be updated with the person who requested the build.
+    - Never
+    - Only if unassigned
+    - Always
